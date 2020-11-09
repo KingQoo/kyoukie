f32bb465ee81e70078536635cc52dca14e8e7020
hiram <hiram@LAPTOP-91U35SK8>
2020-11-09 19:13:21 +0900

    first_commit

---
+++ kyoukie/src/main/java/com/kyoukie/KyoukieApplication.java
@@ -0,0 +1,13 @@
+package com.kyoukie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyoukieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyoukieApplication.class, args);
+	}
+
+}
---
+++ kyoukie/src/test/java/com/kyoukie/KyoukieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyoukie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyoukieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
